e72e008b348c489595229713acc3e4c5f427ff10
b10902118 <b10902118@linux1.csie.ntu.edu.tw>
2025-05-07 00:32:40 +0800

    q1 seeded agent

--- Q1/student_agent.py
+++ Q1/student_agent.py
@@ -1,13 +1,66 @@
 import gymnasium as gym
 import numpy as np
+import torch
+import torch.nn.functional as F
+import torch.nn as nn
 
-# Do not modify the input of the 'act' function and the '__init__' function. 
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+import random
+import numpy as np
+import torch
+
+
+# seems work
+def set_all_seeds(seed: int = 42):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed(seed)
+        torch.cuda.manual_seed_all(seed)  # for multi-GPU setups
+
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+
+
+set_all_seeds(42)
+
+
+# Do not modify the input of the 'act' function and the '__init__' function.
+class Actor(nn.Module):
+    def __init__(self, state_size, action_size, hidden_sizes=[256, 256]):
+        super(Actor, self).__init__()
+        self.net = nn.Sequential(
+            nn.Linear(state_size, hidden_sizes[0]),
+            nn.LayerNorm(hidden_sizes[0]),
+            nn.ReLU(),
+            nn.Linear(hidden_sizes[0], hidden_sizes[1]),
+            nn.LayerNorm(hidden_sizes[1]),
+            nn.ReLU(),
+            nn.Linear(hidden_sizes[1], action_size),
+            nn.Tanh(),  # to ensure output is in [-1, 1]
+        )
+
+    def forward(self, state):
+        return self.net(state)
+
+
+actor = Actor(3, 1)
+actor.load_state_dict(torch.load("actor_learner.pth"))
+actor.to(device)
+
+
 class Agent(object):
     """Agent that acts randomly."""
+
     def __init__(self):
         # Pendulum-v1 has a Box action space with shape (1,)
         # Actions are in the range [-2.0, 2.0]
         self.action_space = gym.spaces.Box(-2.0, 2.0, (1,), np.float32)
 
     def act(self, observation):
-        return self.action_space.sample()
+        state = torch.from_numpy(observation).to(device)
+        with torch.no_grad():
+            return actor(state).cpu().numpy()
