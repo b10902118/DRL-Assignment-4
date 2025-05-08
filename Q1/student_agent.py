ee311e14b8d80bd6d7fda42d77b03333d43b2633
b10902118 <b10902118@linux1.csie.ntu.edu.tw>
2025-05-08 20:31:43 +0800

    remove seeding

--- Q1/student_agent.py
+++ Q1/student_agent.py
@@ -11,23 +11,6 @@ import numpy as np
 import torch
 
 
-# seems work
-def set_all_seeds(seed: int = 42):
-    random.seed(seed)
-    np.random.seed(seed)
-    torch.manual_seed(seed)
-
-    if torch.cuda.is_available():
-        torch.cuda.manual_seed(seed)
-        torch.cuda.manual_seed_all(seed)  # for multi-GPU setups
-
-    torch.backends.cudnn.deterministic = True
-    torch.backends.cudnn.benchmark = False
-
-
-set_all_seeds(42)
-
-
 # Do not modify the input of the 'act' function and the '__init__' function.
 class Actor(nn.Module):
     def __init__(self, state_size, action_size, hidden_sizes=[256, 256]):
