892eb79a3b2a10f0c77d8f56e1ffafd388ab1365
b10902118 <b10902118@linux1.csie.ntu.edu.tw>
2025-05-09 14:40:09 +0800

    try ta's

--- Q3/train.py
+++ Q3/train.py
@@ -1,16 +1,5 @@
-import gymnasium as gym
-import torch
-import torch.nn.functional as F
-import torch.nn as nn
-import torch.optim as optim
-from collections import deque
-import numpy as np
-from torchrl.data import ReplayBuffer, LazyTensorStorage
 from dmc import make_dmc_env
-from tqdm import tqdm
-
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
+import numpy as np
 
 def make_env():
     # Create environment with state observations
@@ -22,6 +11,16 @@ def make_env():
 
 
 env = make_env()
+
+import torch
+import torch.nn.functional as F
+import torch.nn as nn
+import torch.optim as optim
+from collections import deque
+from torchrl.data import ReplayBuffer, LazyTensorStorage
+from tqdm import tqdm
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
 class Actor(nn.Module):
@@ -58,30 +57,30 @@ class Critic(nn.Module):
         return self.net(x)
 
 
-class OUNoise:
-    """Ornstein-Uhlenbeck process for temporally correlated noise."""
-
-    def __init__(self, size, mu=0.0, sigma=0.2, theta=0.15, seed=None):
-        self.mu = mu
-        self.sigma = sigma
-        self.theta = theta
-        self.size = size
-        self.rng = np.random.default_rng(seed)
-        self.reset()
-
-    def reset(self):
-        self.state = np.ones(self.size) * self.mu
-
-    def sample(self):
-        x = self.state
-        dx = self.theta * (self.mu - x) + self.sigma * self.rng.standard_normal(
-            self.size
-        )
-        self.state = x + dx
-        return self.state
-
-
-noise = OUNoise(env.action_space.shape[0], mu=0.0, sigma=0.2, theta=0.15, seed=42)
+#class OUNoise:
+#    """Ornstein-Uhlenbeck process for temporally correlated noise."""
+#
+#    def __init__(self, size, mu=0.0, sigma=0.2, theta=0.15, seed=None):
+#        self.mu = mu
+#        self.sigma = sigma
+#        self.theta = theta
+#        self.size = size
+#        self.rng = np.random.default_rng(seed)
+#        self.reset()
+#
+#    def reset(self):
+#        self.state = np.ones(self.size) * self.mu
+#
+#    def sample(self):
+#        x = self.state
+#        dx = self.theta * (self.mu - x) + self.sigma * self.rng.standard_normal(
+#            self.size
+#        )
+#        self.state = x + dx
+#        return self.state
+#
+#
+#noise = OUNoise(env.action_space.shape[0], mu=0.0, sigma=0.2, theta=0.15, seed=42)
 
 
 def get_action(actor, state, add_noise=True, noise_decay=None):
@@ -89,8 +88,10 @@ def get_action(actor, state, add_noise=True, noise_decay=None):
     with torch.no_grad():
         action = actor(state).cpu().numpy()
     if add_noise:
-        action += noise_decay * noise.sample()
+        #action += noise_decay * noise.sample()
+        action += np.random.normal(0, 0.2, action.shape)
     return np.clip(action, -1, 1)
+
 
 
 def train(
@@ -101,6 +102,7 @@ def train(
     replay_buffer,
     actor_optimizer,
     critic_optimizer,
+    tau = 0.005
 ):
     batch = replay_buffer.sample()
     state, action, reward, next_state, done = (
@@ -128,27 +130,29 @@ def train(
     for target_param, param in zip(
         critic_target.parameters(), critic_learner.parameters()
     ):
-        target_param.data.copy_(target_param.data * 0.99 + param.data * (1.0 - 0.99))
+        target_param.data.copy_(target_param.data * (1-tau) + param.data * tau)
     for target_param, param in zip(
         actor_target.parameters(), actor_learner.parameters()
     ):
-        target_param.data.copy_(target_param.data * 0.99 + param.data * (1.0 - 0.99))
+        target_param.data.copy_(target_param.data * (1-tau) + param.data * tau)
 
 
+NUM_EPISODES = 10000
 TARGET_SCORE = 500
 BATCH_SIZE = 256
 UPDATE_INTERVAL = 25
 PRINT_INTERVAL = 100
+MAX_FALL_DURATION = 100
 
 actor_learner = Actor(67, 21)
 actor_target = Actor(67, 21)
 actor_target.load_state_dict(actor_learner.state_dict())
-actor_optimizer = optim.Adam(actor_learner.parameters(), lr=0.0001)
+actor_optimizer = optim.Adam(actor_learner.parameters(), lr=0.00025)
 
 critic_learner = Critic(67, 21)
 critic_target = Critic(67, 21)
 critic_target.load_state_dict(critic_learner.state_dict())
-critic_optimizer = optim.Adam(critic_learner.parameters(), lr=0.0001)
+critic_optimizer = optim.Adam(critic_learner.parameters(), lr=0.00025)
 
 actor_learner.to(device)
 actor_target.to(device)
@@ -156,16 +160,17 @@ critic_learner.to(device)
 critic_target.to(device)
 
 replay_buffer = ReplayBuffer(
-    storage=LazyTensorStorage(20000, device=device), batch_size=BATCH_SIZE
+    storage=LazyTensorStorage(1000000, device=device), batch_size=BATCH_SIZE
 )
 
 below_target_score = True
-for t in tqdm(range(5000)):
+for t in tqdm(range(NUM_EPISODES)):
     t += 1
     state, _ = env.reset()
     score = 0
     done = False
     step = 0
+    fall_duration = 0
     while not done:  # 1000 steps
         # print(state)
         if t < 50:  # warmup
@@ -175,6 +180,7 @@ for t in tqdm(range(5000)):
                 actor_learner, state, add_noise=True, noise_decay=0.999 ** (t - 1)
             )
         next_state, reward, done, truncated, _ = env.step(action)
+        # print(f"{reward=}") # often < 10**-10
         replay_buffer.add(
             {
                 "state": torch.from_numpy(state).float(),
@@ -184,7 +190,6 @@ for t in tqdm(range(5000)):
                 "done": torch.tensor(done, dtype=torch.int32),
             }
         )
-        done = done or truncated
         if len(replay_buffer) > BATCH_SIZE:
             train(
                 actor_learner,
@@ -198,15 +203,18 @@ for t in tqdm(range(5000)):
         state = next_state
         score += reward
         step += 1
-        if step % UPDATE_INTERVAL == 0:
-            actor_target.load_state_dict(actor_learner.state_dict())
-            critic_target.load_state_dict(critic_learner.state_dict())
+        if reward < 1e-14:
+            fall_duration +=1
+        done = done or truncated or fall_duration > MAX_FALL_DURATION
+        #if step % UPDATE_INTERVAL == 0:
+        #    actor_target.load_state_dict(actor_learner.state_dict())
+        #    critic_target.load_state_dict(critic_learner.state_dict())
 
     # std = np.std(scores_deque)
     # score = mean - std
-    if t % PRINT_INTERVAL == 0:
+    if t % PRINT_INTERVAL == 0 and t > 5000:
         scores = []
-        for i in range(50):  # 1 step/s
+        for i in range(50):  # 1 episode/s
             state, _ = env.reset()
             score = 0
             done = False
